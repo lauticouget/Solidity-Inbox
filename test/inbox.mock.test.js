7ab180efcd1350e048f0117ebcac820b9970a8c5
Lauti <lauticouget@gmail.com>
2020-10-08 11:24:01 -0300

    Lesson 43 | INITIAL COMMIT

---
+++ test/inbox.mock.test.js
@@ -0,0 +1,31 @@
+const assert = require("assert");
+const ganache = require("ganache-cli");
+const Web3 = require("web3");
+const web3 = new Web3(ganache.provider());
+
+"use strict";
+
+class Car {
+  park() {
+    return "stopped";
+  }
+  drive() {
+    return "vroom";
+  }
+}
+
+let car;
+
+beforeEach(() => {
+  car = new Car();
+});
+
+describe("Car", () => {
+  it("can park", () => {
+    assert.strictEqual(car.park(), "stopped");
+  });
+
+  it("can drive", () => {
+    assert.strictEqual(car.drive(), "vroom");
+  });
+});
