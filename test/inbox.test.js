93b82674aa44f0e409367bbde8d63e49a4503382
Lauti <lauticouget@gmail.com>
2020-10-08 12:27:29 -0300

    master | Lesson 45, cleanup code using async/await and variable pre-declaration.

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -3,18 +3,18 @@ const ganache = require("ganache-cli");
 const Web3 = require("web3");
 const web3 = new Web3(ganache.provider());
 
-beforeEach(() => {
-    // Get a list of all ganache test-purpose provided accounts
-    // ASYNC REMEMBER
-    web3.eth.getAccounts()
-    .then(fetchedAccounts => {
-        console.log(fetchedAccounts);
-    });
+let accounts;
 
-    // Use onee of those accounts to deploy contract
+beforeEach(async () => {
+  // Get a list of all ganache test-purpose provided accounts
+  // ASYNC REMEMBER
+  accounts = await web3.eth.getAccounts();
 
+  // Use onee of those accounts to deploy contract
 });
 
-describe('Inbox', () => {
-    it('deploys a contract', () => {});
+describe("Inbox", () => {
+  it("deploys a contract", () => {
+    console.log(accounts);
+  });
 });
