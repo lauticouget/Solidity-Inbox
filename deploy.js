6d4e2e7d1632b47b40d8c345e2dfeda518a62d51
Lauti <lauticouget@gmail.com>
2020-10-27 19:46:20 -0300

    master | Lesson 56, Deployement to Rinkeby

--- deploy.js
+++ deploy.js
@@ -3,5 +3,32 @@ const Web3 = require("web3");
 const { interface, bytecode } = require("./compile");
 
 const provider = new HDWalletProvider(
-    "tourist extra barrel stable depend boat stone need busy solution grape express"
-)
+  "tourist extra barrel stable depend boat stone need busy solution grape express",
+  "https://rinkeby.infura.io/v3/8578c3180ec543099d52f10a44fd1e28"
+);
+const web3 = new Web3(provider);
+
+(async function() {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const instancedContract = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ["This is the initial message"] })
+    .send({ from: accounts[0], gas: 1000000 });
+
+  console.log("Contract deployed to ", instancedContract.options.address);
+}());
+
+// const deploy = async () => {
+//   const accounts = await Web3.eth.getAccounts();
+
+//   console.log("Attempting to deploy from account", accounts[0]);
+
+//   const instancedContract = await new web3.eth.Contract(JSON.parse(interface))
+//     .deploy({ data: bytecode, arguments: ["This is the initial message"] })
+//     .sendTransaction({ from: accounts[0] });
+
+//   console.log("Contract deployed to ", instancedContract.options.address);
+// }
+// deploy();
