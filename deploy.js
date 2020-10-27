eb30dae2eaf64764a72dbdd81aca0abaffe4b8fa
Lauti <lauticouget@gmail.com>
2020-10-27 12:41:51 -0300

    master | Lesson 54, Wallet provider Setup

---
+++ deploy.js
@@ -0,0 +1,7 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require("./compile");
+
+const provider = new HDWalletProvider(
+    "tourist extra barrel stable depend boat stone need busy solution grape express"
+)
