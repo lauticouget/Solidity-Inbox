a9629203046b960358c18c32f6803adf94369cf4
Lauti <lauticouget@gmail.com>
2020-10-27 11:48:27 -0300

    master | Lesson 49, Web3 version fix

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,7 +1,8 @@
 const assert = require("assert");
 const ganache = require("ganache-cli");
 const Web3 = require("web3");
-const web3 = new Web3(ganache.provider());
+const ganacheProvider = ganache.provider();
+const web3 = new Web3(ganacheProvider);
 const { interface, bytecode } = require("../compile");
 
 let accounts;
@@ -16,6 +17,7 @@ beforeEach(async () => {
   inbox = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode, arguments: ["This is the initial message"] })
     .send({ from: accounts[0], gas: 1000000 });
+  inbox.setProvider(ganacheProvider)
 });
 
 describe("Inbox", () => {
